72d9319210238c068e2b5863478512e694228576
Ningxin <Ningxin.Zheng@microsoft.com>
2021-02-24 08:15:46 +0000

    update

--- src/nnfusion/engine/pass/graph/sparse_kernel_pass.cpp
+++ src/nnfusion/engine/pass/graph/sparse_kernel_pass.cpp
@@ -14,12 +14,37 @@
 
 #include "gflags/gflags.h"
 
+DEFINE_bool(sparse_kernel, false, "Sparse Kernel.");
+
 using namespace nnfusion::graph;
 using namespace nnfusion::pass::graph;
 using namespace nnfusion::kernels;
 
+class SparseKernelOptimizer
+{
+public:
+    SparseKernelOptimizer(std::shared_ptr<Graph> g)
+        : m_graph(g)
+    {
+    }
+    bool Optimize(){
+        auto gnodes = m_graph->get_ordered_ops();
+        for(auto node: gnodes){
+            // traverse all nodes and check whether is valuable to
+            // to use the sparse kernel
+
+        }
+        return true;
+    }
+private:
+    std::shared_ptr<Graph> m_graph;
+};
 
 bool SparseKernelPass::run_on_graph(std::shared_ptr<Graph>& graph){
-    
-    return true;
+    bool enable_sparse_kernel = FLAGS_sparse_kernel;
+    if(!enable_sparse_kernel)
+        return true;
+
+    SparseKernelOptimizer optimizer(graph);
+    return optimizer.Optimize();
 }
