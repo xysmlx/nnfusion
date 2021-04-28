552fa38c1abc73f53abfd3589ae1eb4edf08a22f
v-linbin <v-linbin@microsoft.com>
2021-04-28 21:31:47 +0800

    update the conver_external for depthwise conv

--- Exp/inject_kernel.py
+++ Exp/inject_kernel.py
@@ -1,6 +1,14 @@
 import json
 import sys
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--all', default=False, type=bool)
+parser.add_argument('--json', default=None, type=str)
+parser.add_argument('--cu', default=None, type=str)
+args = parser.parse_args()
+
 PREFIX = './kernel'
 files = os.listdir(PREFIX)
 
@@ -15,15 +23,18 @@ def inject(jsonf, cuf):
     with open(jsonf, "w") as f:
         json.dump(data, f)
 
-for f in files:
-    name = os.path.splitext(f)[0]
-    suffix = os.path.splitext(f)[-1]
-    if os.path.splitext(f)[-1]=='.json':
-        jsonf = os.path.join(PREFIX, f)
-        cuf = os.path.join(PREFIX, name+'.cu')
-        assert os.path.exists(jsonf)
-        assert os.path.exists(cuf)
+if args.all:
+    for f in files:
+        name = os.path.splitext(f)[0]
+        suffix = os.path.splitext(f)[-1]
+        if os.path.splitext(f)[-1]=='.json':
+            jsonf = os.path.join(PREFIX, f)
+            cuf = os.path.join(PREFIX, name+'.cu')
+            assert os.path.exists(jsonf)
+            assert os.path.exists(cuf)
 
-        print(jsonf, cuf)
-        inject(jsonf, cuf)
-        os.system("python ../src/tools/nnfusion/kernel_db/convert_external_quantize.py %s"%jsonf)
+            print(jsonf, cuf)
+            inject(jsonf, cuf)
+            os.system("python ../src/tools/nnfusion/kernel_db/convert_external_quantize.py %s"%jsonf)
+else:
+    inject(args.json, args.cu)
